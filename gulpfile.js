26bfe75ce9844f6e53f9baaaccbcd9476f68e641
tvart <vartan.torossian@gmail.com>
2016-09-14 16:28:18 +0200

    start project laravel 5.3 angular 2.6

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var elixir = require('laravel-elixir');
+var elixirTypescript = require('elixir-typescript');
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+//elixir.config.appPath = '';
+elixir(function(mix) {
+    mix.sass('app.scss');
+
+    mix.copy('node_modules/@angular', 'public/libs/@angular');
+    mix.copy('node_modules/rxjs', 'public/libs/rxjs');
+    mix.copy('node_modules/systemjs', 'public/libs/systemjs');
+    mix.copy('node_modules/es6-promise', 'public/libs/es6-promise');
+    mix.copy('node_modules/reflect-metadata', 'public/libs/reflect-metadata');
+    //mix.copy('node_modules/es6-shim', 'public/es6-shim');
+    mix.copy('node_modules/zone.js', 'public/libs/zone.js');
+
+    mix.typescript(
+        [
+            //'*.ts'
+            'app.ts',
+            'module.ts',
+            'main.ts'
+        ],
+        'public/js',
+        {
+            "target": "es6",
+            "module": "system",
+            "moduleResolution": "node",
+            "sourceMap": true,
+            "emitDecoratorMetadata": true,
+            "experimentalDecorators": true,
+            "removeComments": false,
+            "noImplicitAny": false
+        }
+    );
+
+});
---
+++ resources/assets/typescript/main.ts
@@ -0,0 +1,3 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './module';
+platformBrowserDynamic().bootstrapModule(AppModule);
